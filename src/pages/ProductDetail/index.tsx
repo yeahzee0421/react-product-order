b341686bf1961f00e63c6acdd0066f4f58a6c14f
yeahzee0421 <ydw08250421@gmail.com>
2024-07-17 16:12:28 +0900

    feat: 상품 상세 페이지 로딩, 에러 핸들링

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -3,22 +3,33 @@ import { useParams } from 'react-router-dom';
 
 import { useProductDetails } from '@/api/hooks/useGetProductDetails';
 import { Container } from '@/components/common/layouts/Container';
+import RetryErrorBoundary from '@/components/common/RetryErrorBoundary';
+import { Spinner } from '@/components/common/Spinner';
 import { ProductAsideSection } from '@/components/features/Product/ProductAsideSection';
 import { ProductMainSection } from '@/components/features/Product/ProductMainSection';
 
 export const ProductDetailPage = () => {
   const { productId = '' } = useParams<{ productId: string }>();
   const { data, isLoading, isError } = useProductDetails(productId);
-
-  if (isLoading || isError) return null;
-  if (!data) return null;
+  if (isLoading)
+    return (
+      <TextView>
+        <Spinner />
+      </TextView>
+    );
+  if (isError) return <TextView>에러가 발생했습니다.</TextView>;
+  if (!data) return <TextView>해당 상품에 대한 정보가 없습니다.</TextView>;
 
   return (
     <Wrapper>
       <Container maxWidth="1280px" justifyContent="flex-start" alignItems="flex-start">
         <InnerContainer>
-          <ProductMainSection product={data} />
-          <ProductAsideSection product={data} />
+          <RetryErrorBoundary>
+            <ProductMainSection product={data} />
+          </RetryErrorBoundary>
+          <RetryErrorBoundary>
+            <ProductAsideSection product={data} />
+          </RetryErrorBoundary>
         </InnerContainer>
       </Container>
     </Wrapper>
@@ -35,4 +46,13 @@ const InnerContainer = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   position: relative;
+`;
+
+const TextView = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 40px 16px 60px;
+  font-size: 16px;
 `;
