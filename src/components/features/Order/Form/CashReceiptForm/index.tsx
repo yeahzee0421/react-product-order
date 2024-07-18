3182804da4fe276e21cc654d9558b6dfe87d240b
yeahzee0421 <ydw08250421@gmail.com>
2024-07-18 21:46:41 +0900

    feat: 현금영수증 폼 유효성 검증 추가

--- src/components/features/Order/Form/CashReceiptForm/index.tsx
+++ src/components/features/Order/Form/CashReceiptForm/index.tsx
@@ -1,19 +1,75 @@
 import { Checkbox, Input, Select } from '@chakra-ui/react';
+import type { ChangeEvent } from 'react';
+import { useState } from 'react';
 
 import { FormContainer } from '../../OrderAsideSection/component';
 import { Gap } from '../../OrderMainSection/component';
 
-export const CashReceiptForm = () => {
+type CashReceiptFormProps = {
+  onReceiptDataChange: (data: CashReceiptData) => void;
+};
+
+type CashReceiptData = {
+  isReceiptChecked: boolean;
+  receiptNumber: string;
+};
+
+export const CashReceiptForm = ({ onReceiptDataChange }: CashReceiptFormProps) => {
+  const [cashReceiptData, setCashReceiptData] = useState<CashReceiptData>({
+    isReceiptChecked: false,
+    receiptNumber: '',
+  });
+
+  const handleCheckBoxChange = () => {
+    const checkBoxEvent = !cashReceiptData.isReceiptChecked;
+    setCashReceiptData((prev) => {
+      const newData = { ...prev, isReceiptChecked: checkBoxEvent };
+      onReceiptDataChange(newData);
+      return newData;
+    });
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const receiptNumber = e.target.value;
+    setCashReceiptData((prev) => {
+      const newData = { ...prev, receiptNumber: receiptNumber };
+      onReceiptDataChange(newData);
+      validateReceiptForm(newData);
+      return newData;
+    });
+  };
+
   return (
     <FormContainer>
-      <Checkbox colorScheme="orange">현금영수증 신청</Checkbox>
+      <Checkbox
+        colorScheme="orange"
+        isChecked={cashReceiptData.isReceiptChecked}
+        onChange={handleCheckBoxChange}
+      >
+        현금영수증 신청
+      </Checkbox>
       <Gap />
       <Select name="cashReceiptType">
         <option value="PERSONAL">개인소득공제</option>
         <option value="BUSINESS">사업자증빙용</option>
       </Select>
       <div style={{ height: '8px' }} />
-      <Input name="cashReceiptNumber" placeholder="(-없이) 숫자만 입력해주세요." />
+      <Input
+        name="cashReceiptNumber"
+        placeholder="(-없이) 숫자만 입력해주세요."
+        onChange={handleInputChange}
+      />
     </FormContainer>
   );
+};
+
+export const validateReceiptForm = (data: CashReceiptData) => {
+  let receiptError = '';
+  if (data.isReceiptChecked && data.receiptNumber.trim().length === 0) {
+    receiptError = '현금영수증 번호를 입력해주세요';
+  }
+  if (!/^\d*$/.test(data.receiptNumber)) {
+    receiptError = '현금영수증 번호는 숫자로만 입력해주세요.';
+  }
+  return receiptError;
 };
--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,3 +1,4 @@
+import { Divider } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
@@ -5,18 +6,23 @@ import { useLocation } from 'react-router-dom';
 import { Container } from '@/components/common/layouts/Container';
 import { Aside, AsideContainer, Main, MainContainer } from '@/components/common/layouts/Split';
 import {
+  CashReceiptForm,
+  validateReceiptForm,
+} from '@/components/features/Order/Form/CashReceiptForm';
+import {
   MessageFormSection,
   validateMessageForm,
 } from '@/components/features/Order/Form/MessageFormSection';
 import { OrderAsideSection } from '@/components/features/Order/OrderAsideSection';
 import { OrderMainSection } from '@/components/features/Order/OrderMainSection';
 import { storageOrderHistory } from '@/components/features/Order/util/storage';
-import { CashReceiptForm } from '@/components/features/Order/Form/CashReceiptForm';
-import { Divider } from '@chakra-ui/react';
 
 type FormData = {
   message: string;
-  receiptNumber: string;
+  cashReceiptData: {
+    isReceiptChecked: boolean;
+    receiptNumber: string;
+  };
 };
 
 export const OrderPage = () => {
@@ -25,23 +31,31 @@ export const OrderPage = () => {
   const productId = location.state.product.id;
   const quantity = productData.quantity;
 
-  const getTotalCost = () => {
-    const sellingPrice = productData.product.detail.price.sellingPrice;
-    return sellingPrice * quantity;
-  };
-
-  const [formData, setFormData] = useState<FormData>({ message: '', receiptNumber: '' });
+  const [formData, setFormData] = useState<FormData>({
+    message: '',
+    cashReceiptData: { isReceiptChecked: false, receiptNumber: '' },
+  });
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const messageError = validateMessageForm(formData.message);
+    const receiptError = validateReceiptForm(formData.cashReceiptData);
 
     if (messageError) {
       alert(`${messageError}`);
       return;
     }
+    if (receiptError) {
+      alert(`${receiptError}`);
+      return;
+    }
     storageOrderHistory(productId);
     alert('주문이 완료되었습니다.');
+  };
+
+  const getTotalCost = () => {
+    const sellingPrice = productData.product.detail.price.sellingPrice;
+    return sellingPrice * quantity;
   };
 
   return (
@@ -63,7 +77,11 @@ export const OrderPage = () => {
                   <span className="span-title">결제 정보</span>
                 </h6>
                 <Divider aria-orientation="horizontal" />
-                <CashReceiptForm />
+                <CashReceiptForm
+                  onReceiptDataChange={(cashReceiptData) =>
+                    setFormData({ ...formData, cashReceiptData })
+                  }
+                />
                 <OrderAsideSection totalCost={getTotalCost()} />
               </AsideContainer>
             </Aside>
